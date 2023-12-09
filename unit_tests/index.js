5c9eb438910f383c494b31e17615c880ae7c61c9
Tuomas Henriksson <tuomas@Tuomass-Mac-mini.local>
2023-12-09 15:03:36 +0200

    add tests

--- unit_tests/index.js
+++ unit_tests/index.js
@@ -5,6 +5,8 @@ import isEmpty from '../components/isEmpty.js';
 import toInteger from '../components/toInteger.js';
 import toNumber from '../components/toNumber.js';
 import toString from '../components/toString.js';
+import toFinite from '../components/toFinite.js'
+import eq from '../components/eq.js'
 
 
 const users = [
@@ -178,3 +180,77 @@ describe('toNumber', () => {
     });
   });
 });
+
+describe('toFinite', () => { 
+  describe('Convert to finite number', function () { 
+    it('must return finite number', () => { 
+      expect(0).to.be.deep.equal(toFinite(0)); 
+    }); 
+  }); 
+  describe('Convert to finite number', function () { 
+    it('must return finite number', () => { 
+      expect(5.2).to.be.deep.equal(toFinite(5.2)); 
+    }); 
+  }); 
+  describe('Convert to finite number', function () { 
+    it('must return finite number', () => { 
+      expect(1).to.be.deep.equal(toFinite(true)); 
+    }); 
+  }); 
+  describe('Convert to finite number', function () { 
+    it('must return finite number', () => { 
+      expect(0).to.be.deep.equal(toFinite(undefined)); 
+    }); 
+  }); 
+  describe('Convert to finite number', function () { 
+    it('must return finite number', () => { 
+      expect(2.4).to.be.deep.equal(toFinite('2.4')); 
+    }); 
+  }); 
+}); 
+ 
+describe('eq', () => { 
+  describe('Checks if both values are equal', function () { 
+    it('must return true', () => { 
+      expect(true).to.be.deep.equal(eq(5, 5)); 
+    }); 
+  }); 
+  describe('Checks if both values are equal', function () { 
+    it('must return false', () => { 
+      expect(false).to.be.deep.equal(eq(3.1, 3)); 
+    }); 
+  }); 
+   
+  describe('Checks if both values are equal', function () { 
+    it('must return true', () => { 
+      expect(true).to.be.deep.equal(eq(4, '4')); 
+    }); 
+  }); 
+  describe('Checks if both values are equal', function () { 
+    it('must return false', () => { 
+      expect(false).to.be.deep.equal(eq(1, null)); 
+    }); 
+  }); 
+  describe('Checks if both values are equal', function () { 
+    it('must return false', () => { 
+      expect(false).to.be.deep.equal(eq(null, NaN)); 
+    }); 
+  }); 
+  describe('Checks if both values are equal', function () { 
+    it('must return false', () => { 
+      expect(false).to.be.deep.equal(eq(undefined, 2)); 
+    }); 
+  }); 
+  describe('Checks if both values are equal', function () { 
+    it('must return false', () => { 
+      expect(false).to.be.deep.equal(eq(-6, 6)); 
+    }); 
+  }); 
+ 
+  describe('Checks if both values are equal', function () { 
+    it('must return true', () => { 
+      expect(true).to.be.deep.equal(eq(undefined, undefined)); 
+    }); 
+  }); 
+}); 
+
